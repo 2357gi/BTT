cb938df392d95db32f20dadc0bc97e4fbf63199b
2357gi <2357.gi@gmail.com>
2018-11-29 11:56:43 +0900

    add scripts

---
+++ scripts/formtest.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+import time
+import sys
+import random
+
+i = random.randrange(5)
+
+if i == 1:
+    print("sushi tabetai🍣🍣🍣")
+elif i == 2:
+    print("niku tabetai🍖🍖🍖")
+elif i == 3:
+    print("nabe tabetai🥘🥘🥘")
+elif i == 2:
+    print("sakana tabetai🐟🐟🐟")
+else:
+   print("ebi tabetai🦐🦐🦐🦐")
